3675f2f94f248892598c7437b7da18086b5ded2f
DevRhysLy <rhysproduction@gmail.com>
2021-05-23 22:17:21 +1000

    contact test

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -7,6 +7,24 @@ import Call from '../components/Call';
 const Contact = ({ data }) => {
   const { title } = data.markdownRemark.frontmatter;
   const { html } = data.markdownRemark;
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        ...name
+      })
+    }).then(() => navigate("/thank-you/")).catch(error => alert(error))
+  }
   return (
     <Layout bodyClass="page-default-single">
       <div className="container pb-6 pt-6 pt-md-10 pb-md-10">
@@ -14,30 +32,28 @@ const Contact = ({ data }) => {
           <div className="col-12 col-md-8">
             <h1 className="title">{title}</h1>
             <div className="content mt-4" dangerouslySetInnerHTML={{ __html: html }} />
-            <form name="contact" method="POST" data-netlify="true">
-              <p>
-                <label className="form-control">Your Name: <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label className="custom-control-label">Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label className="custom-control-label">Closest Studio: <select name="Closest Studio" multiple>
-                  <option value="Croydon">Croydon</option>
-                  <option value="Ermington West">Ermington West</option>
-                  <option value="Belrose">Belrose</option>
-                  <option value="West Hoxton">West Hoxton</option>
-                  <option value="Yarrawarrah">Yarrawarrah</option>
-                </select></label>
-              </p>
-              <p>
-                <label>Message: <textarea name="message"></textarea></label>
-              </p>
-              <div data-netlify-recaptha="true">
-              </div>
-              <p>
-                <button className="btn" type="submit">Send</button>
-              </p>
+            <form
+              name="contact-form"
+              method="post"
+              data-netlify="true"
+              data-netlify-recaptcha="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="contact-form" value="contact-form" />
+              <label>Name: <input name="name" placeholder="John Citizen" type="text" /></label>
+              <label>Email: <input name="email" placeholder="name@name.com" type="email" /></label>
+              <label>Studio: 
+              <select>
+                <option value="Croydon">Croydon</option>
+                <option value="Ermington West">Ermington West</option>
+                <option selected value="Belrose">Belrose</option>
+                <option value="West Hoxton">West Hoxton</option>
+                <option selected value="Yarrawarrah">Yarrawarrah</option>
+              </select>
+              </label>
+              <label>Enquiry: <textarea name="message" /></label>
+              <div data-netlify-recaptcha="true"></div>
+              <button>Send</button>
             </form>
           </div>
         </div>
